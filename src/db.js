b4d8aea6e44f8473c40db0213d3c7d45c773fc84
Leticia <leeh2003val@gmail.com>
2021-09-09 15:17:31 -0300

    ok

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import initdb from './models/init-models.js'
 import Sequelize from 'sequelize';
 const sequelize = new Sequelize(
-'N5MnijCqZkfOME_BD',
-'5MnijCqZkf',
-'cXNvMIr556', {
-host: 'remotemysql.com',
+'mysql_17753_nsf2021',
+'nsf2021',
+'nsf@2021', {
+host: 'my01.winhost.com',
 dialect: 'mysql',
 logging: false
 });
